13760039dbcdaf961e4a78b7c17929c501461335
David Tyler <davet.code@googlemail.com>
2012-02-04 14:28:09 +0000

    Starting to build up the project structure

---
+++ src/main/scala/tyler/breakout/input/InputHandler.scala
@@ -0,0 +1,8 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: David
+ * Date: 04/02/12
+ * Time: 12:29
+ * To change this template use File | Settings | File Templates.
+ */
+
---
+++ src/main/scala/tyler/breakout/messaging/MessagePassing.scala
@@ -0,0 +1,19 @@
+package tyler.breakout.messaging
+
+/**
+ * User: David
+ * Date: 04/02/12
+ * Time: 13:03
+ */
+
+object MessagePassing {
+
+
+  def Register(component: Any, message: Message) {
+
+  }
+
+  def Receive(message: Message) {
+
+  }
+}
